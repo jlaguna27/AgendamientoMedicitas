5a8b2946f897563b60ca4945ad687da7a4ae5f33
Juan <Sebastian>
2019-06-20 15:15:25 -0500

    Ajustando Linea de static url

--- agendamientocitas/urls.py
+++ agendamientocitas/urls.py
@@ -52,9 +52,7 @@ urlpatterns = [
 
     path('login', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
